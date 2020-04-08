5fa389aa1ce7781ee7a595f7125166b9a40fe741
Tobiasz Maj <tobiaszmaj1@gmail.com>
2020-04-08 22:29:28 +0200

    Adding Google Maps

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ https:// <br>
 Used technologies: HTML, CSS/SASS, JavaScript, RWD, Flexbox, NPM. <br>
 Used JS plugins: <br>
 - Flickity - carousel <br>
+- Mustache - template <br>
+- Google Maps - Google API Console <br>
--- js/main.js
+++ js/main.js
@@ -1,23 +1,69 @@
+"use strict";
+
+// mustache
+var templateCarousel = document.getElementById("carousel-template").innerHTML;
 var carousel = document.querySelector('.main-carousel');
-var flkty = new Flickity( carousel, {
-//options
+var carouselElements = "";
+
+for (var i = 0; i < slideData.length; i++) {
+  carouselElements += Mustache.render(templateCarousel, slideData[i]);
+}
+
+carousel.innerHTML = carouselElements;
+
+// flickity carousel
+var flkty = new Flickity(carousel, {
+  //options
   cellAlign: "left",
   contain: true,
   pageDots: false, // remove page dots
   hash: true // change page url after slide change
 });
 
-// Restart button - return to first slide
-document
-  .querySelector(".restart-button")
-  .addEventListener("click", function() {
-    flkty.selectCell(".start");
-  });
+// restart button
+var restart = document.querySelector(".restart-button");
 
-// Progress bar below carousel   
+restart.addEventListener("click", function () {
+  flkty.select(0);
+});
+
+// progress bar
 var progressBar = document.querySelector(".progress-bar");
 
-flkty.on("scroll", function(progress) {
+flkty.on("scroll", function (progress) {
   progress = Math.max(0, Math.min(1, progress));
   progressBar.style.width = progress * 100 + "%";
 });
+
+// google maps
+(function () {
+  window.initMap = function () {
+
+    var mapLocation = slideData[0].coords;
+
+    // zoom and center
+    var map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 6,
+      center: mapLocation
+    });
+
+    // 
+    var markers = [];
+    for (var i = 0; i < slideData.length; i++) {
+      markers.push(new google.maps.Marker({
+        position: slideData[i].coords,
+        map: map,
+        id: i
+      }))
+      markers[i].addListener("click", function () {
+        flkty.select(this.id)
+      });
+
+      // change map after slide
+      flkty.on("change", function (index) {
+        map.panTo(slideData[index].coords);
+        map.setZoom(6);
+      });
+    }
+  };
+})();
