c7310485b777417bc96ec23e84253a61897750d1
Tobiasz Maj <tobiaszmaj1@gmail.com>
2020-04-09 18:47:00 +0200

    Moving slideData to new js file

---
+++ js/slideData.js
@@ -0,0 +1,48 @@
+"use strict";
+
+var slideData = [{
+        image: "images/los_angeles.jpg",
+        title: "Los Angeles",
+        description: "<p>center of the film industry in sunny California</p>",
+        coords: {
+            lat: 34.052235,
+            lng: -118.243683
+        }
+    },
+    {
+        image: "images/moscow.jpg",
+        title: "Moscow",
+        description: "<p>combination of modernity and relics of communism</p>",
+        coords: {
+            lat: 55.751244,
+            lng: 37.618423
+        }
+    },
+    {
+        image: "images/sydney.jpg",
+        title: "Sydney",
+        description: "<p>gateway to the world of windsurfing and... kangaroos</p>",
+        coords: {
+            lat: -33.865143,
+            lng: 151.209900
+        }
+    },
+    {
+        image: "images/london.jpg",
+        title: "London",
+        description: "<p>Europe's largest financial center</p>",
+        coords: {
+            lat: 51.509865,
+            lng: -0.118092
+        }
+    },
+    {
+        image: "images/tokyo.jpg",
+        title: "Tokyo",
+        description: "<p>high-tech Tokyo</p>",
+        coords: {
+            lat: 35.652832,
+            lng: 139.839478
+        }
+    },
+]
